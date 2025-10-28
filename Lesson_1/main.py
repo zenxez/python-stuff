401dbd2ef2de3c35e7b2ecc78828aa410aa268a9
Luka Goderdzishvili <lukagoderdzishvili4@gmail.com>
2025-10-28 14:47:51 +0400

    Added Lesson_1 folder

---
+++ Lesson_1/main.py
@@ -0,0 +1,70 @@
+# name = "John" # String -> str
+# name2 = 'John has hotel "John home"' # String -> str
+
+# age = 20 # Integer -> int
+# height = 1.75 # Float -> float
+# is_student = True # Boolean -> bool
+# is_business = False
+
+# print("Hello,", name)
+# print("Hello, " + name)
+# print(f"Hello, {name}")
+
+#  ------------------------------------
+# x = 10
+# y = 87
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+# print(x % y)
+# print(x // y)
+# print(x ** y)
+#  ------------------------------------
+
+# x = 10
+# print(type(x))
+# string_x = str(x)
+# print(string_x)
+# print(type(string_x))
+
+# number = "1234"
+# print(type(number))
+# int_number = int(number)
+# print(int_number)
+# print(type(int_number))
+
+# float_str = "12345"
+# print(type(float_str))
+# float_number = float(float_str)
+# print(float_number)
+# print(type(float_number))
+
+# is_student_str = "True"
+# print(type(is_student_str))
+# is_student_bool = bool(is_student_str)
+# print(is_student_bool)
+# print(type(is_student_bool))
+
+#  ------------------------------------
+
+# name = "John"
+# age = 20
+# print("John is", age, "years old")
+
+#  ------------------------------------
+# name = input("Enter your name: ")
+# print(f"Hello, {name}.")
+# print(type(name))
+# age = input("Enter your age: ")
+# age = int(age)
+# print(f"You are {age} years old. you will be {age + 5} in 5 years.")
+# print(type(age))
+# height = float(input("Enter your height: "))
+# print(f"You are {height} meters tall.")
+# print(type(height))
+
+# is_student = bool(input("Are you a student? (True/False): "))
+# print(f"You are a student: {is_student}.")
+# print(type(is_student))
+
