2a4f83cc5da704e4a058d19faee7971eec1af6a8
Luka Goderdzishvili <lukagoderdzishvili4@gmail.com>
2025-10-28 20:50:54 +0400

    Added Lesson 2 folder

---
+++ Lesson_2/main.py
@@ -0,0 +1,59 @@
+# age = int(input("Enter your age: "))
+# print(age == 18)
+# print(age != 18)
+# print(age > 18)
+# print(age < 18)
+# print(age >= 18)
+# print(age <= 18)
+
+# ------------------------------------
+# age = int(input("Enter your age: "))
+# print(age >= 18)
+# print(age == 17)
+# if  age >= 18:
+#     print("You are eligible to vote")
+# elif age == 17:
+#     print("You will be eligible to vote next year")
+# else:
+#     print("You are not eligible to vote")
+# ------------------------------------
+
+# has_ticket = input("Do you have a ticket? (yes/no): ")
+# is_vip = input("Are you a VIP? (yes/no): ")
+
+# if has_ticket == "yes" or is_vip == "yes":
+#     print("You are allowed to enter the stadium")
+# else:
+#     print("You are not allowed to enter the stadium")
+
+# have_license = input("Do you have a license? (yes/no): ")
+# have_car = input("Do you have a car? (yes/no): ")
+# have_license = have_license == "yes"
+# have_car = have_car == "yes"
+# print(have_license, have_car)
+
+# if have_license and have_car:
+#     print("You are allowed to drive")
+# elif have_license and not have_car:
+#     print("You are not allowed to drive because you don't have a car")
+# elif not have_license and have_car:
+#     print("You are not allowed to drive because you don't have a license")
+# else:
+#     print("You are not allowed to drive because you don't have a license and a car")
+
+# ------------------------------------
+
+# age = 17
+# has_license = True
+# has_car = True
+
+# if age >= 18:
+#     if has_license:
+#         if has_car:
+#             print("You are 18+ years old.\nYou have a license and a car")
+#         else:
+#             print("You are 18+ years old.\nYou have a license but you don't have a car")
+#     else:
+#         print("You are 18+ years old.\nYou don't have a license")
+# else:
+#     print("You are not 18+ years old")
