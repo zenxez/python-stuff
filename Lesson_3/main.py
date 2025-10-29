d275e2d5ac31673524780aeace89d13b85fa7410
Luka Goderdzishvili <lukagoderdzishvili4@gmail.com>
2025-10-29 20:35:59 +0400

    Added Lesson 3 Folder

---
+++ Lesson_3/main.py
@@ -0,0 +1,69 @@
+# range(5) -> 0, 1, 2, 3, 4
+# for i in range(5):
+#     print(i)
+
+# --------------------------------
+
+# range(1, 10) -> 1, 2, 3, 4, 5, 6, 7, 8, 9
+# for i in range(1, 10):
+#     print(f"{i}. Hello")
+
+# --------------------------------
+
+# range(1, 15, 5) -> 1, 6, 11
+# for i in range(1, 15, 5):
+#     print(i)
+
+# --------------------------------
+
+
+#break -> stop the loop
+# for i in range(1, 15):
+#     if i == 5:
+#         break
+#     print(i)
+
+
+#continue -> skip the current iteration
+# for i in range(1, 15):
+#     if i == 5:
+#         continue
+#     print(i)
+
+# --------------------------------
+
+# num = 1
+# while num <= 10:
+#     print(num)
+#     num = num + 1 # num += 1
+
+# is_five = False
+# sum = 0
+# while not is_five:
+#     if sum == 5:
+#         is_five = True
+#     else:
+#         sum += 1
+#         print(sum)
+
+# --------------------------------
+
+# for i in range(3):
+#     for j in range(2):
+#         print(f"i: {i}, j: {j}")
+
+# --------------------------------
+
+# for i  in range(1, 6):
+#     if i == 3:
+#         break
+#     print(i)
+# else:
+#     print("Loop finished!")
+
+# --------------------------------
+
+# word = "Python"
+
+# for letter in word:
+#     print(letter)
