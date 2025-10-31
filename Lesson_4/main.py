d9ac0b8aa1180dc857398e66f7cf4dc635b76e95
Luka Goderdzishvili <lukagoderdzishvili4@gmail.com>
2025-10-31 21:37:48 +0400

    Added Lesson_4 Folder

---
+++ Lesson_4/main.py
@@ -0,0 +1,63 @@
+# fruits = ["apple", "banana", "cherry"]
+# # aplle -> 0 , banana -> 1 , cherry -> 2
+# element_0 = fruits[-3]
+# print(element_0)
+
+# ------------Change elements------------
+# fruits = ["apple", "banana", "cherry"]
+# print(fruits[1])
+# fruits[1] = "orange"
+# print(fruits[1])
+# print(fruits)
+
+# ------------Add elements------------
+# fruits = ["apple", "banana", "cherry"]
+# print(fruits)
+# fruits.append("orange")
+# print(fruits)
+# fruits.insert(2, "Mango")
+# print(fruits)
+
+
+
+
+# ------------Remove elements------------
+# names = ["Ana", "Luka", "Nika"]
+# print(names)
+# # names.remove("Ana")
+# # print(names)
+# # names.pop(0)
+# # print(names)
+# # del names[0]
+# # print(names)
+# names.clear()
+# print(names)
+
+# ------------Clear list------------
+# names = ["Ana", "Luka", "Nika"]
+
+# for i, name in enumerate(names):
+#     print(f"{i}. {name}")
+
+# ------------ length ------------
+# names = ["Ana", "Luka", "Nika"]
+# print(len(names))
+
+# -----------------------------------
+# fruits = ["apple", "banana", "cherry"]
+# fruits_2 = ["Mango", "Orange"]
+# print(fruits)
+# print(fruits_2)
+# fruits_3 = fruits + fruits_2
+# print(fruits_3)
+
+# actors = ["Tom Hanks", "Meryl Streep"]
+# actors_10_times = actors * 10
+# print(actors_10_times)
+
+
+# ----------------------------------
+
+# actors = ["Tom Hanks", "Meryl Streep", "Brad Pitt", "Angelina Jolie", "Brad Pitt"]
+# print(actors.index("Brad Pitt"))
+# print(actors.count("Brad Pitt"))
